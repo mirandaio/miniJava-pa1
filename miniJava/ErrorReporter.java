ec39341fac5546e755c251cf55101c58651cfb90
Alfredo Miranda <alfredo@miranda.io>
2013-08-10 12:34:24 -0500

    Add ErrorReporter.java

---
+++ miniJava/ErrorReporter.java
@@ -0,0 +1,29 @@
+package miniJava;
+
+import miniJava.SyntacticAnalyzer.SourcePosition;
+
+public class ErrorReporter {
+
+    int numErrors;
+
+    ErrorReporter() {
+        numErrors = 0;
+    }
+
+    public void reportError(String message, String tokenName, 
+                            SourcePosition pos) {
+        System.out.print("ERROR: ");
+
+        for(int p = 0; p < message.length(); p++)
+            if(message.charAt(p) == '%')
+                System.out.print(tokenName);
+            else
+                System.out.print(message.charAt(p));
+        System.out.println(" " + pos.start + ".." + pos.finish);
+        numErrors++;
+    }
+
+    public void reportRestriction(String message) {
+        System.out.println("RESTRICTION: " + message);
+    }
+}
